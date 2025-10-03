202f1f411a067dd6f98ae26aff2fbbd736102f02
Khenya <abrendakhenya@gmail.com>
2025-10-03 11:12:00 -0400

    feat: add QR code generation functionality

---
+++ scripts/QR.js
@@ -0,0 +1,20 @@
+const QRCode = require("qrcode");
+const fs = require("fs");
+
+const sessionId = "asistencia_entrada";
+
+QRCode.toFile(
+  "./qr_asistencia.png",
+  sessionId,
+  {
+    color: {
+      dark: "#14518f", 
+      light: "#FFF" // blanco
+    },
+    width: 300
+  },
+  function (err) {
+    if (err) throw err;
+    console.log("✅ QR generado: qr_asistencia.png");
+  }
+);
